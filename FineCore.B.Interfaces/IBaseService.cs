32599ef3359c09e09d4221e966b098d39f58c68f
Administrator <Administrator@SERVER>
2020-01-01 00:10:04 +0800

    B. BaseService; B.Interfaces.IBaseService

---
+++ FineCore.B.Interfaces/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace FineCore.B.Interfaces {
+    [ServiceContract]
+    public interface IBaseService {
+
+        [OperationContract]
+        bool CheckLogin(string userKeyField, string userKeyValue, string loginPwd);
+
+    }
+}
--- FineCore.B/BaseService.cs
+++ FineCore.B/BaseService.cs
@@ -1,6 +1,17 @@
-﻿using System;
+﻿using FineCore.B.Interfaces;
+using System;
 
 namespace FineCore.B {
-    public abstract class BaseService {
+    public abstract class BaseService : IBaseService {
+        /// <summary>
+        /// 检验登陆
+        /// </summary>
+        /// <param name="userKeyField">账号类型（映射的数据库字段）</param>
+        /// <param name="userKeyValue">账号</param>
+        /// <param name="loginPwd">密码</param>
+        /// <returns>是否登入</returns>
+        public bool CheckLogin(string userKeyField, string userKeyValue, string loginPwd) {
+            throw new NotImplementedException();
+        }
     }
 }

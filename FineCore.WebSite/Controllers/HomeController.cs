3ea32601a050ed62b1d2b33768597f4ce69892b1
jack sun <jack.sun@andfox.cn>
2020-01-02 17:31:57 +0800

    更新WebSite中对vue引用

--- FineCore.WebSite/Controllers/HomeController.cs
+++ FineCore.WebSite/Controllers/HomeController.cs
@@ -6,21 +6,13 @@ using Microsoft.Extensions.Logging;
 namespace FineCore.WebSite.Controllers {
     public partial class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
-        private readonly ILoggerFactory _loggerFactory;
 
-        //public HomeController(ILogger<HomeController> logger) {
-        //    _logger = logger;
-        //}
-
-        public HomeController(ILogger<HomeController> logger, ILoggerFactory loggerFactory) {
-            _logger = logger;
-            _loggerFactory = loggerFactory;
-        }
+        public HomeController(ILogger<HomeController> logger) { _logger = logger; }
 
         public IActionResult Index() {
-            var dbProvider = FineCore.DB.DbSettings.GetDbProvider();
             _logger.LogWarning("Hi, 这是Log4net日志。");
-            _loggerFactory.CreateLogger<HomeController>().LogWarning("This is HomeController-Index 1");
+            var dbProvider = FineCore.DB.DbSettings.GetDbProvider();
+            _logger.LogWarning($"{dbProvider}");
             return View();
         }
 
